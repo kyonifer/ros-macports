932fef5199fcf6719cad5623ac6b1057a9e988be
Kyle Kauffman <kyle.j.kauffman@gmail.com>
2013-10-08 09:52:58 -0400

    Make directory for generated ports generic.

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 A simple script to take a set of packages in a ROS workspace and conver them
@@ -7,6 +7,7 @@ into a set of macports ports with a fully working dependency tree.
 """
 
 import os
+import sys
 from xml.dom import minidom as xml
 
 
@@ -24,19 +25,22 @@ if __name__ == "__main__":
     pkgs = [pkg for pkg in os.listdir(".") if
             not pkg.startswith(".") and os.path.isdir(pkg)]
     prefix = "ros-hydro-"
+    portdir = sys.argv[1] if len(sys.argv) > 1 else "../ports"
     try:
-        os.mkdir("../ports")
+        os.mkdir(portdir)
     except OSError:
         pass
     for pkg in pkgs:
         pkgname = prefix + pkg
         manifest = xml.parse(pkg + "/package.xml")
-        build_deps = [prefix+ele.lastChild.nodeValue for ele in
+        build_deps = [prefix + ele.lastChild.nodeValue for ele in
                       manifest.getElementsByTagName("build_depend")]
+        if pkg != "catkin":
+            build_deps.append(prefix + "catkin")
         build_deps_str = " \\\n                    port:".join(build_deps)
         if build_deps_str is not "":
             build_deps_str = "\\\n                    port:" + build_deps_str
-        run_deps = [prefix+ele.lastChild.nodeValue for ele in
+        run_deps = [prefix + ele.lastChild.nodeValue for ele in
                     manifest.getElementsByTagName("run_depend")]
         run_deps_str = " \\\n                    port:".join(run_deps)
         if run_deps_str is not "":
@@ -49,7 +53,7 @@ if __name__ == "__main__":
         template = template.replace("$$run_depends$$", run_deps_str)
         template = template.replace("$$build_depends$$", build_deps_str)
         try:
-            os.mkdir("../ports/" + pkgname)
+            os.mkdir(portdir + "/" + pkgname)
         except OSError:
             pass
-        writefile("../ports/" + pkgname + "/Portfile", template)
+        writefile(portdir + "/" + pkgname + "/Portfile", template)
